7d3512fde06ba23783d3011c0de5d52942c2ac69
goleodel <leodelfini2005@gmail.com>
2026-05-18 17:10:22 +0200

    Update main_draft.cpp

--- main_draft.cpp
+++ main_draft.cpp
@@ -1,3 +1,8 @@
+/*! \file main_draft.cpp
+    \brief testare le classi create
+	\author Delfini-Iannone
+*/
+
 #include <iostream>
 #include <cmath>
 
@@ -10,6 +15,8 @@ using namespace std;
 
 #define MAX_SHAPES 10
 
+
+/// @brief per verificare il corretto funzionamento del programma 
 int main()
 {
     Shape* shapes[MAX_SHAPES];
@@ -17,33 +24,43 @@ int main()
 
     cout << "===== TEST GERARCHIA SHAPE =====" << endl;
 
-    // 1. Creazione figure
-    shapes[nShapes++] = new Rectangle(0, 0, 10, 5,0.5);
-    shapes[nShapes++] = new Rectangle(0, 0, 10, 5,1);
-    shapes[nShapes++] = new Rhombus(2, 2, 10, 6,0.5);
+    /// 1. Creazione figure (l'ultimo numero è la scala(vengono create al 100% cioè sf=1))
+    shapes[nShapes++] = new Rectangle(0, 0, 10, 5, 1);
     shapes[nShapes++] = new Rhombus(2, 2, 10, 6, 1);
-    shapes[nShapes++] = new RightTriangle (6, 8, 3, 5, 0.5);
     shapes[nShapes++] = new RightTriangle (6, 8, 3, 5, 1);
 
-    // 2. Impostazione testo
+
+    /// 2. Impostazione testo
     shapes[0]->SetText("rettangolo");
-    shapes[1]->SetText("rettangolo");
-    shapes[2]->SetText("rombo");
-    shapes[3]->SetText("rombo");
-    shapes[4]->SetText("Triangolo");
-    shapes[5]->SetText("Triangolo");
+    shapes[1]->SetText("rombo");
+    shapes[2]->SetText("Triangolo");
 
 
-    // 3. Dump polimorfico
+
+    /// 3. Dump polimorfico
     cout << endl << "===== DUMP POLIMORFICO =====" << endl;
-
     for (int i = 0; i < nShapes; i++) {
         cout << endl << "Figura [" << i << "]" << endl;
         shapes[i]->Dump();
     }
 
-    
 
+    /// 4. test della funzione Scale
+    cout << endl << "===== SCALA LA FIGURA =====" << endl;
+    for (int i = 0; i < nShapes; i++) {
+        shapes[i]-> Scale(0.5);
+    }
+
+    /// 5. Dump con scala
+    cout << endl << "===== DUMP SCALATO =====" << endl;
+    for (int i = 0; i < nShapes; i++) {
+        cout << endl << "Figura [" << i << "]" << endl;
+        shapes[i]->Dump();
+    }
+
+
+
+    /// 6. distruzione delle classi
     cout << endl << "===== FINE TEST =====" << endl;
     for (int i = 0; i < nShapes; i++) {
         delete shapes[i];
